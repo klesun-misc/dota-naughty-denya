4fcefa55416fb5fb9a0874fc9a218de0d2be3704
Artur Klesun <arturklesun@gmail.com>
2017-11-19 16:10:36 +0000

    Make barracks always phased so they did not block path to creeps

--- game/dota_addons/green_envy_xmas_defense/scripts/vscripts/abilities/build_melee_barracks.lua
+++ game/dota_addons/green_envy_xmas_defense/scripts/vscripts/abilities/build_melee_barracks.lua
@@ -35,7 +35,9 @@ build_melee_barracks = build_tower_base.MakeAbility({
 
         -- need to apply any modifier to update npc gui hp numbers
         tower:AddNewModifier(nil, nil, 'modifier_stunned', {duration = 0.05})
+        tower:AddNewModifier(nil, nil, 'modifier_phased', {duration = -1})
         tower:SetControllableByPlayer(abil:GetCaster():GetMainControllingPlayer(), false)
+        tower:SetHullRadius(-1)
     end,
     ---@param abil CDOTABaseAbility
     CustomPointCheck = function(point, abil)
